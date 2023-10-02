3d97e7da8a3962056097db43dc74b1d0303dc0f4
bola-nabil <bolanabil404@gmail.com>
2023-10-02 18:43:18 +0300

    add projec files

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <div className='contanier'>
+                <div className='main'>
+                    <p><span>HI, </span>I'm Bola Nabil</p>
+                    <img src="mainPhoto.jpg" alt="not found"/>
+                </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
