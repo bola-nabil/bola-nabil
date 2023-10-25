32377fd38864da9ebbdc2ade05966bce552deae6
bola-nabil <bolanabil404@gmail.com>
2023-10-25 12:45:09 +0300

    Update Files in Project

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,10 +6,9 @@ class App extends Component {
         return (
             <React.Fragment>
                 <div className='contanier'>
-                <div className='main'>
-                    <p><span>HI, </span>I'm Bola Nabil</p>
-                    <img src="mainPhoto.jpg" alt="not found"/>
-                </div>
+                    <div className='main'>
+                        <p><span>HI, </span>I'm Bola Nabil</p>
+                    </div>
                 </div>
             </React.Fragment>
         );
