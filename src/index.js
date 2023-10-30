eb07a569b9da701177b24f83b16d848ec35b9e02
bola-nabil <bolanabil404@gmail.com>
2023-10-30 18:07:37 +0200

    add folder photos to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/main.css'
 import reportWebVitals from './reportWebVitals';
-import App from './components/app';
+import App from './pages/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
