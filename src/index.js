8770fbc5d277bc2567db465ffe7a9fcee6a9e7fb
bola-nabil <bolanabil404@gmail.com>
2023-10-30 21:06:58 +0200

    update index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './css/main.css'
 import reportWebVitals from './reportWebVitals';
-import App from './pages/App';
-
+// import Home from './pages/Home';
+import App from './pages/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
