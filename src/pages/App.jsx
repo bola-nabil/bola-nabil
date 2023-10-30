fd25086e23c8654f5b2ce93dc28092799fffae6e
bola-nabil <bolanabil404@gmail.com>
2023-10-30 21:04:07 +0200

    update app.jsx file

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Resume from './Resume';
+import Projects from './Projects';
+import Contact from './Contact';
 
 class App extends Component {
     state = {  } 
     render() { 
         return (
             <React.Fragment>
-                <div className='contanier'>
-                    <div className='main'>
-                        <p><span>HI, </span>I'm Bola Nabil</p>
-                    </div>
-                </div>
+                <Routes>
+                    <Route path='/' element={<Home/>}></Route>
+                    <Route path='/about' element={<About/>}></Route>
+                    <Route path='/resume' element={<Resume/>}></Route>
+                    <Route path='/projects' element={<Projects/>}></Route>
+                    <Route path='/contact' element={<Contact/>}></Route>
+                </Routes>
             </React.Fragment>
         );
     }
