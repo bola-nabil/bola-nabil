625d6baf37c82898d09faf44c34aa8501ca79275
bola-nabil <bolanabil404@gmail.com>
2023-10-31 20:45:19 +0200

    update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends Component {
     state = {  } 
@@ -9,33 +15,46 @@ class Home extends Component {
                 <div className='home'>
                     <div className='header'>
                         <div className='container'>
-                            <div className='logo'>
-                                <p>Bola</p>
-                            </div>
+                            <div className='logo'>bola-nabil</div>
                             <nav>
                                 <div className='box'>
                                     <li>
-                                        <Link to='/'>Home</Link>
+                                        <Link className='headLink active' to='/'>
+                                            <FontAwesomeIcon className='navIcon' icon={faHouse} />
+                                            Home
+                                        </Link>
                                     </li>
                                 </div>
                                 <div className='box'>
                                     <li>
-                                        <Link to='/about'>About</Link>
+                                        <Link className='headLink' to='/about'>
+                                            <FontAwesomeIcon className='navIcon' icon={faUser} />
+                                            About
+                                        </Link>
                                     </li>
                                 </div>
                                 <div className='box'>
                                     <li>
-                                        <Link to='/resume'>Resume</Link>
+                                        <Link className='headLink' to='/resume'>
+                                            <FontAwesomeIcon className='navIcon' icon={faFile} />
+                                            Resume
+                                        </Link>
                                     </li>
                                 </div>
                                 <div className='box'>
                                     <li>
-                                        <Link to='/projects'>Projects</Link>
+                                        <Link className='headLink' to='/projects'>
+                                            <FontAwesomeIcon className='navIcon' icon={faListCheck} />
+                                            Projects
+                                        </Link>
                                     </li>
                                 </div>
                                 <div className='box'>
                                     <li>
-                                        <Link to='/contact'>Contact</Link>
+                                        <Link className='headLink' to='/contact'>
+                                            <FontAwesomeIcon className='navIcon' icon={faPhone} />
+                                            Contact
+                                        </Link>
                                     </li>
                                 </div>
                             </nav>
