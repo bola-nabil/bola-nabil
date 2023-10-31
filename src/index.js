61b1284a85ef4244b40a4672e8e2d9d7195a4886
bola-nabil <bolanabil404@gmail.com>
2023-10-31 20:43:55 +0200

    update index.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 // import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './css/main.css'
+
+import './css/main.css';
+// import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import reportWebVitals from './reportWebVitals';
 // import Home from './pages/Home';
 import App from './pages/app';
