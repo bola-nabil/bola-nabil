2166a9b7a3b65d16a75f6482bfa397e862bc500e
bola-nabil <bolanabil404@gmail.com>
2023-11-01 17:03:25 +0200

    update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
 import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 class Home extends Component {
     state = {  } 
@@ -13,6 +17,7 @@ class Home extends Component {
         return (
             <React.Fragment>
                 <div className='home'>
+                    <div className='overlay'></div>
                     <div className='header'>
                         <div className='container'>
                             <div className='logo'>bola-nabil</div>
@@ -59,6 +64,51 @@ class Home extends Component {
                                 </div>
                             </nav>
                         </div>
+                        <section>
+                            <div className='image'>
+                                <img src={require("../photos/myPhoto.jpg")} alt="not found" />
+                            </div>
+                            <div className='content'>
+                                <div className='intro'>
+                                    <h1>Bola Nabil</h1>
+                                    <p className='title'>I'm a <span>Software Engineer</span></p>
+                                    <p className='about'>I'm a Web Designer. over 3 years of experience.
+                                    Experienced with all stages of the development cycle for dynamic web projects.</p>
+                                </div>
+                                <div className='aboutLinks'>
+                                    <ul>
+                                        <li>
+                                            <Link className='titleLink' to='https://facebook.com/bolanabil404' target='_blank'>
+                                                <FontAwesomeIcon className='titleIcon' icon={faFacebook} />
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to='https://twitter.com/bolaNabil_' target='_blank'>
+                                                <FontAwesomeIcon className='titleIcon' icon={faTwitter} />
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to='https://linkedin.com/in/bolanabil' target='_blank'>
+                                                <FontAwesomeIcon className='titleIcon' icon={faLinkedin} />
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to='https://github.com/bola-nabil' target='_blank'>
+                                                <FontAwesomeIcon className='titleIcon' icon={faGithub} />
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className='contactBox'>
+                                    <div className='contactMe'>
+                                        <Link to='/contact'>Contact Me</Link>
+                                    </div>
+                                    <div className='myProjects'>
+                                        <Link to='/projects'>My Projects</Link>
+                                    </div>
+                                </div>
+                            </div>
+                    </section>
                     </div>
                 </div>
             </React.Fragment>
