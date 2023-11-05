faaafa1fb6b4d1908cbb3696dbea4544fd494d4a
bola-nabil <bolanabil404@gmail.com>
2023-11-05 20:08:26 +0200

    update Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -43,7 +43,30 @@ class Contact extends Component {
                             <Link className='mailContact' to='mailto:bolanabil404@gmail.com'>bolanabil404@gmail.com</Link>
                         </div>
                     </div>
+                    <div className='contactMessage'>
+                        <div className='messageBox'>
+                            <form action="mailto:bolanabil404@gmail.com">
+                                <div className='infoForm'>
+                                    <input type="text" placeholder='Name*' required/>
+                                    <input type="email" placeholder='Email*' required/>
+                                </div>
+                                <div className='subject'>
+                                    <input type="text" placeholder='Subject*'/>
+                                </div>
+                                    <textarea placeholder='Message*' required></textarea>
+                                <div className='submit'>
+                                    <input type="submit" value='Send Message' />
+                                </div>
+                            </form>
+                        </div>
+                        <div className='mapBox'>
+                            <iframe title='location' src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d258.44339217918514!2d30.90221538442333!3d29.36484403875814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1699171481734!5m2!1sar!2seg"></iframe>
+                        </div>
+                    </div>
                 </div>
+                <footer>
+                    <p>&copy; 2023 Bola Nabil  -  All rights reserved.</p>
+                </footer>
             </div>
         );
     }
