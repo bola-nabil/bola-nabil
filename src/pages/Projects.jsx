0c823a5fa179830fc5cf1ab2d98d55c0ae68148e
bola-nabil <bolanabil404@gmail.com>
2023-11-06 21:34:14 +0200

    update Project file

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 class Projects extends Component {
     state = {  } 
@@ -12,6 +16,44 @@ class Projects extends Component {
                             <div className='insideText'>
                                 <span className='first'>My </span>
                                 <span className='second'>Projects</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='projectSection'>
+                        <div className='projectBox'>
+                            {/* <div className='projectLinks'>
+                                <Link className='userLink' to='https://www.github.com/bola-nabil' target='_blank'>
+                                    <div className='projectIcon'>
+                                        <FontAwesomeIcon className='icon' icon={faLink} />
+                                    </div>
+                                </Link>
+                                <Link className='userLink' to='https://www.github.com/bola-nabil' target='_blank'>
+                                    <div className='projectIcon'>
+                                        <FontAwesomeIcon className='icon' icon={faGithub} />
+                                    </div>
+                                </Link>
+                            </div> */}
+
+                            <div className='projectImage'>
+                                {/* <img src={require("../photos/projects/calculator_project.jpg")} alt="not found" /> */}
+                            </div>
+                        </div>
+                        <div className='projectBox'>
+                            <div className='projectLinks'>
+                                    <Link to='https://www.github.com/bola-nabil' target='_blank'></Link>
+                                    <Link to='https://www.github.com/bola-nabil' target='_blank'></Link>
+                            </div>
+                            <div className='projectImage'>
+                                <img src={require("../photos/projects/calculator_project.jpg")} alt="not found" />
+                            </div>
+                        </div>
+                        <div className='projectBox'>
+                            <div className='projectLinks'>
+                                    <Link to='https://www.github.com/bola-nabil' target='_blank'></Link>
+                                    <Link to='https://www.github.com/bola-nabil' target='_blank'></Link>
+                            </div>
+                            <div className='projectImage'>
+                                <img src={require("../photos/projects/calculator_project.jpg")} alt="not found" />
                             </div>
                         </div>
                     </div>
