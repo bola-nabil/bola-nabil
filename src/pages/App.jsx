0bb7fd7916d6892fe3742ec52443d8db2487fd7e
bola-nabil <bolanabil404@gmail.com>
2024-02-05 14:52:01 +0200

    update app page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,8 +4,7 @@ import Resume from './Resume';
 import Projects from './Projects';
 import Contact from './Contact';
 import NotFound from './NotFound';
-
-import { Link } from "react-router-dom";
+import NavLink from '../components/NavLink';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
@@ -41,72 +40,72 @@ const App = () => {
 }, []);
 
 return (
-    <div className='x'>
-    <div className={`header ${isNavFixed ? 'fixed' : ''}`}>
-        <div className='container'>
-            <div className='logo'>
-                <p>bola-nabil</p>
-            </div>
-            <nav className={`${showMenu ? 'show' : ''}`}>
-                <ul>
-                    <li onClick={() =>  handleItemClick('home') } className={activeItem === 'home' ? 'active' : ''} >
-                        <Link onClick={() => setShowMenu(!showMenu)} to='/bola-nabil' className='homeLinks'>
-                            <div className='homeIcon'>
-                                <FontAwesomeIcon icon={faHouse}/>
-                            </div>
-                            Home
-                        </Link>
-                    </li>
-                    <li onClick={() =>  handleItemClick('about') } className={activeItem === 'about' ? 'active' : ''} >
-                        <Link onClick={() => {setShowMenu(!showMenu); handleItemClick('about');}} to='/about' className='homeLinks'>
-                            <div className='homeIcon'>
-                                <FontAwesomeIcon icon={faUser}/>
-                            </div>
-                            About
-                        </Link>
-                    </li>
-                    <li onClick={() => handleItemClick('resume') } className={activeItem === 'resume' ? 'active' : ''} >
-                        <Link onClick={() => setShowMenu(!showMenu)} to='/resume' className='homeLinks'>
-                            <div className='homeIcon'>
-                                <FontAwesomeIcon icon={faFile}/>
-                            </div>
-                            Resume
-                        </Link>
-                    </li>
-                    <li onClick={() => handleItemClick('projects')} className={activeItem === 'projects' ? 'active' : ''} >
-                        <Link onClick={() => setShowMenu(!showMenu)} to='/projects' className='homeLinks'>
-                            <div className='homeIcon'>
-                                <FontAwesomeIcon icon={faListCheck}/>
-                            </div>
-                            Projects
-                        </Link>
-                    </li>
-                    <li onClick={() => handleItemClick('contact')} className={activeItem === 'contact' ? 'active' : ''} >
-                        <Link onClick={() => setShowMenu(!showMenu)} to='/contact' className='homeLinks'>
-                            <div className='homeIcon'>
-                                <FontAwesomeIcon icon={faPhone}/>
-                            </div>
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-                <div className={`bars`} onClick={() => setShowMenu(!showMenu)}>
-                    <FontAwesomeIcon className='barsIcon' icon={faBars} />
-                </div>
-            </div>
-</div>
+  <div className="x">
+    <div className={`header ${isNavFixed ? "fixed" : ""}`}>
+      <div className="container">
+        <div className="logo">
+          <p>bola-nabil</p>
+        </div>
+        <nav className={`${showMenu ? "show" : ""}`}>
+          <ul>
+            <NavLink
+              onClick={() => handleItemClick("home")}
+              active={activeItem === "home" ? "active" : ""}
+              showMenuClick={() => setShowMenu(!showMenu)}
+              to="/bola-nabil"
+              icon={faHouse}
+              title="Home"
+            />
+            <NavLink
+              onClick={() => handleItemClick("about")}
+              active={activeItem === "about" ? "active" : ""}
+              showMenuClick={() => setShowMenu(!showMenu)}
+              to="/about"
+              icon={faUser}
+              title="About"
+            />
+            <NavLink
+              onClick={() => handleItemClick("resume")}
+              active={activeItem === "resume" ? "active" : ""}
+              showMenuClick={() => setShowMenu(!showMenu)}
+              to="/resume"
+              icon={faFile}
+              title="Resume"
+            />
+            <NavLink
+              onClick={() => handleItemClick("projects")}
+              active={activeItem === "projects" ? "active" : ""}
+              showMenuClick={() => setShowMenu(!showMenu)}
+              to="/projects"
+              icon={faListCheck}
+              title="Projects"
+            />
+            <NavLink
+              onClick={() => handleItemClick("contact")}
+              active={activeItem === "contact" ? "active" : ""}
+              showMenuClick={() => setShowMenu(!showMenu)}
+              to="/contact"
+              icon={faPhone}
+              title="Contact"
+            />
+          </ul>
+        </nav>
+        <div className={`bars`} onClick={() => setShowMenu(!showMenu)}>
+          <FontAwesomeIcon className="barsIcon" icon={faBars} />
+        </div>
+      </div>
+    </div>
 
     <Routes>
-        <Route path='/bola-nabil'  exact element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/resume' element={<Resume/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='*' element={<NotFound/>} />
+      <Route path="/bola-nabil" exact element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-    </div>  
-    );
+  </div>
+);
 };
 
 export default App;
