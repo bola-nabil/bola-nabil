0900aa68cdd639ef105afb0c789015ee812dc572
bola-nabil <bolanabil404@gmail.com>
2024-02-05 14:48:59 +0200

    add Information component at about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,22 +2,13 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Footer from './Footer';
 import PageTitle from '../components/PageTitle';
+import Information from '../components/Information';
 
 const About = () => {
-    function theAge() {
-        const today = new Date();
-        const birthDate = new Date("2001-03-10");
-        let age = today.getFullYear() - birthDate.getFullYear();
-        const monthDiff = today.getMonth() - birthDate.getMonth();
-        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
-        return age;
-    }
 return (
   <div className="about">
     <div className="container">
-      <PageTitle title="About" first="About " second="Me"/>
+      <PageTitle title="About" first="About " second="Me" />
       <div className="aboutSection">
         <div className="boxImage">
           <img src={require("../photos/myPhoto.jpg")} alt="not found" />
@@ -54,98 +45,46 @@ return (
       <div className="aboutInfo">
         <div className="calOne">
           <ul>
-            <li>
-              <label>Birthday: </label>
-              <span className="desInfo">10 Mar, 2001</span>
-            </li>
-            <li>
-              <label>City: </label>
-              <span className="desInfo">Al-Fayoum, Egypt</span>
-            </li>
-            <li>
-              <label>Study: </label>
-              <span className="desInfo">Akhbar El Youm Academy</span>
-            </li>
-            <li>
-              <label>Website: </label>
-              <span className="desInfo">
-                <Link
-                  className="infoLink"
-                  to="https://bola-nabil.github.io/bola-nabil"
-                  target="_blank"
-                >
-                  bola-nabil.github.io/bola-nabil
-                </Link>
-              </span>
-            </li>
-            <li>
-              <label>Phone: </label>
-              <span className="desInfo infoLink">(+20) 102 614 6442</span>
-            </li>
-            <li>
-              <label>Github: </label>
-              <span className="desInfo">
-                <Link
-                  className="infoLink"
-                  to="https://www.github.com/bola-nabil"
-                  target="_blank"
-                >
-                  bola-nabil
-                </Link>
-              </span>
-            </li>
+            <Information title="Birthday: " info="10 Mar, 2001" />
+            <Information title="City: " info="Al-Fayoum, Egypt" />
+            <Information title="Study: " info="Akhbar El Youm Academy" />
+            <Information
+              title="Protfolio: "
+              to="https://bola-nabil.github.io/bola-nabil"
+              LinkTitle="bola-nabil.github.io/bola-nabil"
+            />
+            <Information
+              title="Phone: "
+              info="(+20) 102 614 6442"
+              className="infoLink"
+            />
+            <Information
+              title="Github: "
+              to="https://www.github.com/bola-nabil"
+              LinkTitle="bola-nabil"
+            />
           </ul>
         </div>
         <div className="calTwo">
           <ul>
-            <li>
-              <label>Age: </label>
-              <span className="desInfo">{theAge()}</span>
-            </li>
-            <li>
-              <label>Experience: </label>
-              <span className="desInfo">0-1 Years</span>
-            </li>
-            <li>
-              <label>Degree: </label>
-              <span className="desInfo">Bachelor of Computer Science</span>
-            </li>
-            <li>
-              <label>Email: </label>
-              <span className="desInfo">
-                <Link
-                  className="infoLink"
-                  to="mailto:bolanabil404@gmail.com"
-                  target="_blank"
-                >
-                  bolanabil404@gmail.com
-                </Link>
-              </span>
-            </li>
-            <li>
-              <label>Hackerrank: </label>
-              <span className="desInfo">
-                <Link
-                  className="infoLink"
-                  to="https://www.hackerrank.com/profile/bolaNabil"
-                  target="_blank"
-                >
-                  bolaNabil
-                </Link>
-              </span>
-            </li>
-            <li>
-              <label>leetcode: </label>
-              <span className="desInfo">
-                <Link
-                  className="infoLink"
-                  to="https://www.leetcode.com/bola-nabil"
-                  target="_blank"
-                >
-                  bola-nabil
-                </Link>
-              </span>
-            </li>
+            <Information title="Age: " info={theAge()} />
+            <Information title="Experience: " info="0-1 Years" />
+            <Information title="Degree: " info="Bachelor of Computer Science" />
+            <Information
+              title="Email: "
+              to="mailto:bolanabil404@gmail.com"
+              LinkTitle="bolanabil404@gmail.com"
+            />
+            <Information
+              title="Hackerrank: "
+              to="https://www.hackerrank.com/profile/bolaNabil"
+              LinkTitle="bolaNabil"
+            />
+            <Information
+              title="leetcode: "
+              to="https://www.leetcode.com/bola-nabil"
+              LinkTitle="bola-nabil"
+            />
           </ul>
         </div>
       </div>
@@ -156,3 +95,17 @@ return (
 };
 
 export default About;
+
+function theAge() {
+    const today = new Date();
+    const birthDate = new Date("2001-03-10");
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+    return age;
+}
