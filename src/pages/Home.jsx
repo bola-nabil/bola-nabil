25e1aa37233a5bd014ed3390cd457d061818adcc
bola-nabil <bolanabil404@gmail.com>
2024-02-05 14:49:39 +0200

    add HomeLink component in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,50 @@
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import HomeLink from "../components/HomeLink";
 
 const Home = () => {
 return (
-    <div className='homePage'>
-        <div className="overlay"></div>
-        <div className='homeSection'>
-            <div className='homeContent'>
-                <div className='homeImage'>
-                    <img src={require('../photos/myPhoto.jpg')} alt="not found" />
-                </div>
-                <div className='aboutSection'>
-                    <div className='homeTitle'>
-                        <h2>Bola Nabil</h2>
-                        <span>I'm a </span>
-                        <span className='second'>Software Engineer</span>
-                        <p>I'm a Frontend Developer. 0 - 1 years of experience. Experienced with all  dynamic web projects.</p>
-                    <div className='hLinks'>
-                        <li>
-                            <Link to='https://www.facebook.com/bolanabil404' target='_blank'>
-                                <div className='homeIcon'>
-                                    <FontAwesomeIcon className='icon' icon={faFacebookF} />
-                                </div>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='https://www.twitter.com/bolaNabil_' target='_blank'>
-                                <div className='homeIcon'>
-                                    <FontAwesomeIcon className='icon' icon={faXTwitter} />
-                                </div>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='https://www.linkedin.com/in/bolanabil' target='_blank'>
-                                <div className='homeIcon'>
-                                    <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
-                                </div>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='https://www.github.com/bola-nabil' target='_blank'>
-                                <div className='homeIcon'>
-                                    <FontAwesomeIcon className='icon' icon={faGithub}/>
-                                </div>
-                            </Link>
-                        </li>
-                    </div>
-                </div>
-                </div>
-            </div>
+  <div className="homePage">
+    <div className="overlay"></div>
+    <div className="homeSection">
+      <div className="homeContent">
+        <div className="homeImage">
+          <img src={require("../photos/myPhoto.jpg")} alt="not found" />
         </div>
+        <div className="aboutSection">
+          <div className="homeTitle">
+            <h2>Bola Nabil</h2>
+            <span>I'm a </span>
+            <span className="second">Software Engineer</span>
+            <p>
+              I'm a Frontend Developer. 0 - 1 years of experience. Experienced
+              with all dynamic web projects.
+            </p>
+            <div className="hLinks">
+              <HomeLink
+                to="https://www.facebook.com/bolanabil404"
+                icon={faFacebookF}
+              />
+              <HomeLink
+                to="https://www.twitter.com/bolaNabil_"
+                icon={faXTwitter}
+              />
+              <HomeLink
+                to="https://www.linkedin.com/in/bolanabil"
+                icon={faLinkedinIn}
+              />
+              <HomeLink
+                to="https://www.github.com/bola-nabil"
+                icon={faGithub}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 );
 };
 
