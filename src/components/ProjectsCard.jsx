3e43dee8a89ef7032b038765575e4a8ec9c6b98f
bola-nabil <bolanabil404@gmail.com>
2024-03-04 17:00:06 +0200

    Update ProjectCard file

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,29 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
 function ProjectsCard(props) {
   return (
     <>
-      <div className="projectBox">
-        <div className="projectImage">
-            <img src={props.src} alt={props.alt}/>
-        </div>
-        <div className="projectTitle">
-          <h2>{props.title}</h2>
-        </div>
-        <div className="projectLinks">
-          <Link to={props.toSite} target="_blank">
-            <div className={props.className}>
-              <FontAwesomeIcon className="icon" icon={props.iconSite}/>
-            </div>
-          </Link>
-          <Link to={props.toGit} target="_blank">
-            <div className="projectIcon">
-              <FontAwesomeIcon className="icon" icon={props.iconGit} />
-            </div>
-          </Link>
-        </div>
-      </div>
+      <Card style={{ width: "20rem" }} border="danger" className="project-move">
+        <Card.Img variant="top" src={props.src} alt={props.alt} />
+        <Card.Body>
+          <Card.Title className="text-center fw-bold pb-2">{props.title}</Card.Title>
+          <div className="d-flex justify-content-center">
+            <Link to={props.toSite} target="_blank">
+              <div className={props.className}>
+                <FontAwesomeIcon className="icon" icon={props.iconSite} />
+              </div>
+            </Link>
+            <Link to={props.toGit} target="_blank">
+              <div className="projectIcon">
+                <FontAwesomeIcon className="icon" icon={props.iconGit} />
+              </div>
+            </Link>
+          </div>
+        </Card.Body>
+      </Card>
     </>
   );
 }
