b01a7e2c02aaf88d0cfb1e006537d79eb7511824
Bola Nabil <bolanabil404@gmail.com>
2025-11-03 19:59:00 +0200

    add bootstrap styles

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return ( 
-        <div className='not-found'>
-            <h2>404 - Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
+        <div className='not-found text-center d-flex vh-100 justify-content-center align-items-center flex-column'>
+            <h2 className='fw-bold fs-3 my-4'>404 - Not Found</h2>
+            <p className='fs-5 fw-bold'>The page you are looking for does not exist.</p>
         </div>
     );
 }
