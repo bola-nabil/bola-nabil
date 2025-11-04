bd38cf1da188ec494dca0ebcc2a020a85971dd92
Bola Nabil <bolanabil404@gmail.com>
2025-11-04 15:46:09 +0200

    update basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="bola-nabil">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
