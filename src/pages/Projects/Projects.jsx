2764bd1edde3e399358c00e1a92663303c056931
Bola Nabil <bolanabil404@gmail.com>
2026-01-27 15:21:53 +0200

    change names of classes and improve calling data

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import useData  from "../../hooks/useData";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Footer from "../../components/layout/Footer/Footer";
+import PageTitle from "../../components/ui/PageTitle";
+import ProjectsCard from "../../components/ProjectsCard";
+import { Row, Col } from "react-bootstrap";
+import "./projects.css";
+
+const Projects = () => {
+    const data = useData();
+
+  const pageSize = 9;
+  const [currentPage, setCurrentPage] = useState(0);
+  const totalPages = Math.ceil(data?.projects?.length / pageSize);
+  const nextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+  };
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
+  const startIndex = currentPage * pageSize;
+  const endIndex = startIndex + pageSize;
+
+
+  return (
+    <div className="projects">
+      <div className="container">
+        <PageTitle title="Projects" first="My " second="Projects" />
+        <div className="mt-4">
+          <Row sm="2" lg="3">
+            {data?.projects
+              ?.map((project, index) => (
+                <Col
+                  key={index}
+                  className="mt-4 d-flex justify-content-center align-items-center"
+                >
+                  <ProjectsCard
+                    src={require(`../../assets/images/projects/${project.imgUrl}`)}
+                    alt="not found"
+                    title={project.title}
+                    toSite={project.liveLink}
+                    iconSite={faLink}
+                    className="project-icon"
+                    toGit={project.gitLink}
+                    iconGit={faGithub}
+                  />
+                </Col>
+              ))
+              .slice(startIndex, endIndex)}
+          </Row>
+          <div className="mt-4 text-center">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 0}
+              className="btn btn-danger"
+            >
+              &lt;
+            </button>
+            <span className="ps-3 pe-3">{currentPage + 1}</span>
+            <button
+              onClick={nextPage}
+              disabled={currentPage === totalPages - 1}
+              className="btn btn-danger"
+            >
+              &gt;
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
