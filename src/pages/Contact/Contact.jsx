3792b9f38c9357ae71617e5dcbb93b165fd8e903
Bola Nabil <bolanabil404@gmail.com>
2026-01-27 15:23:19 +0200

    change names of classes and improve calling data

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import Footer from "../../components/layout/Footer/Footer";
+import PageTitle from "../../components/ui/PageTitle";
+import ContactCard from "../../components/ContactCard";
+import  useData from "../../hooks/useData";
+import "./contact.css";
+
+const Contact = () => {
+  const data = useData();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("https://bola-nabil.github.io/bola-nabil", {
+        to: email,
+        subject,
+        text: message,
+      });
+
+      setIsSubmitted(true);
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+  };
+
+  return (
+    <div className="contact">
+      <div className="container">
+        <PageTitle title="Contact" first="Get In " second="Touch" />
+
+        <div className="contact-section">
+          <ContactCard
+            className="contact-location d-flex flex-column justify-content-center align-items-center position-relative p-4"
+            icon={faLocationDot}
+            title="My Location:"
+            description={data?.city}
+          />
+          <ContactCard
+            className="contact-phone d-flex flex-column justify-content-center align-items-center position-relative p-4"
+            icon={faPhone}
+            title="Phone Number:"
+            classContact="phoneNumber"
+            description={data?.contact?.phone}
+          />
+          <div className="contact-email d-flex flex-column justify-content-center align-items-center position-relative p-4">
+            <div className="contact-icon">
+              <FontAwesomeIcon className="icon" icon={faEnvelope} />
+            </div>
+            <h2>Email Address:</h2>
+            <Link
+              className="mail-contact"
+              to={`mailto:${data?.contact?.email}`}
+            >
+              {data?.contact?.email}
+            </Link>
+          </div>
+        </div>
+        <div className="contact-message">
+          <div className="message-box">
+            <form onSubmit={handleSubmit}>
+              <div className="info-form">
+                <input
+                  type="text"
+                  placeholder="Name*"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+                <input
+                  type="email"
+                  placeholder="Email*"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="subject">
+                <input
+                  type="text"
+                  placeholder="Subject*"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                  required
+                />
+              </div>
+              <textarea
+                className="w-100"
+                placeholder="Message*"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+              <div className="submit">
+                <input type="submit" value="Send Message" />
+              </div>
+            </form>
+            {isSubmitted && (
+              <div style={{ color: "green", marginTop: "10px" }}>
+                Success! The form was submitted successfully.
+              </div>
+            )}
+          </div>
+          <div className="map-box">
+            <iframe
+              title="location"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d258.44339217918514!2d30.90221538442333!3d29.36484403875814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1699171481734!5m2!1sar!2seg"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
