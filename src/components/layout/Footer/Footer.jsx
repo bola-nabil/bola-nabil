cd7182bb88c1f838d7c5596ca0eae3b4b4f8bd6b
Bola Nabil <bolanabil404@gmail.com>
2026-01-27 15:28:10 +0200

    layout include navbar and footer and other layouts

---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { currentYear } from "../../../utilts/currentYear";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="position-relative left-0 bottom-0">
+      <div className="footer-content w-100 d-flex justify-content-center align-items-center position-relative flex-column bg-black">
+        <p className="m-auto fs-5">&copy; {currentYear()} Bola Nabil - All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
