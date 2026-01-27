584186e39b846f7f6daba174b7ee57abb4ccb1c6
Bola Nabil <bolanabil404@gmail.com>
2026-01-27 15:29:04 +0200

    make components include skills code and styles

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Skills = ({ title, status, style }) => {
+  return (
+    <div className="skill">
+      <h3>
+        {title}
+        <span>{status}</span>
+      </h3>
+      <div className="the-progress">
+        <span style={{ width: style }}></span>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
