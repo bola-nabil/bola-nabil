cf4143ec8a93c27e869be671177b0332ff58d39c
Bola Nabil <bolanabil404@gmail.com>
2026-01-31 14:53:36 +0200

    add map of projects to inlcude images

---
+++ src/utilts/projectsMap.js
@@ -0,0 +1,51 @@
+import aagento from "../assets/images/projects/aagento.png";
+import blognest from "../assets/images/projects/blognest.png";
+import blognestApis from "../assets/images/projects/blognest-apis.png";
+import ebsar from "../assets/images/projects/ebsar.png";
+import ebsarApis from "../assets/images/projects/ebsar-apis.png";
+import clock from "../assets/images/projects/clock.png";
+import drumMachine from "../assets/images/projects/drum-machine.png";
+import randomQuoteMachine from "../assets/images/projects/random-quote-machine.png";
+import technoit from "../assets/images/projects/technoit.png";
+import portfolioTemplate from "../assets/images/projects/portfolio-template.png";
+import cosmix from "../assets/images/projects/cosmix.png";
+import movieApp from "../assets/images/projects/movie-app.jpg";
+import interviews from "../assets/images/projects/interviews.jpg";
+import myPortfolio from "../assets/images/projects/my-portfolio.jpg";
+import kasper from "../assets/images/projects/kasper.jpg";
+import dashboard from "../assets/images/projects/dashboard.jpg";
+import portfolio from "../assets/images/projects/portfolio.jpg";
+import leon from "../assets/images/projects/leon.jpg";
+import calculator from "../assets/images/projects/calculator_project.jpg";
+import bankSystem from "../assets/images/projects/bank-system.jpg";
+import leetcode from "../assets/images/projects/leetcode.jpg";
+import codeforces from "../assets/images/projects/codeforces.jpg";
+import atcoder from "../assets/images/projects/atcoder.jpg";
+
+
+
+export const projectsMap = {
+    "aagento.png": aagento,
+    "blognest.png": blognest,
+    "blognest-apis.png": blognestApis,
+    "ebsar.png": ebsar,
+    "ebsar-apis.png": ebsarApis,
+    "clock.png": clock,
+    "projects/drum-machine.png": drumMachine,
+    "random-quote-machine.png": randomQuoteMachine,
+    "technoit.png": technoit,
+    "portfolio-template.png": portfolioTemplate,
+    "cosmix.png": cosmix,
+    "movie-app.jpg": movieApp,
+    "interviews.jpg": interviews,
+    "my-portfolio.jpg": myPortfolio,
+    "kasper.jpg": kasper,
+    "dashboard.jpg": dashboard,
+    "portfolio.jpg": portfolio,
+    "leon.jpg": leon,
+    "calculator_project.jpg": calculator,
+    "bank-system.jpg": bankSystem,
+    "leetcode.jpg": leetcode,
+    "codeforces.jpg": codeforces,
+    "atcoder.jpg": atcoder
+}
