3f3b031218d998ff0373ec61ab5eef67bb75324b
Bola Nabil <bolanabil404@gmail.com>
2026-01-31 14:02:16 +0200

    add map of certificates images to manage images

---
+++ src/utilts/certificatesMap.js
@@ -0,0 +1,24 @@
+import cert1 from "../assets/images/certificate/icpc2021.jpg";
+import cert2 from "../assets/images/certificate/icpc2021x.jpg";
+import cert3 from "../assets/images/certificate/icpc2022.jpg";
+import cert4 from "../assets/images/certificate/icpc2022x.jpg";
+import cert5 from "../assets/images/certificate/javascript.jpg";
+import cert6 from "../assets/images/certificate/css.jpg";
+import cert7 from "../assets/images/certificate/freelancer.jpg";
+import cert8 from "../assets/images/certificate/responsive-freecodecamp.png";
+import cert9 from "../assets/images/certificate/javascript-freecodecamp.png";
+import cert10 from "../assets/images/certificate/frontend-freecodecamp.png";
+
+
+export const certificatesMap  = {
+    "icpc2021.jpg": cert1,
+    "icpc2021x.jpg": cert2,
+    "icpc2022.jpg": cert3,
+    "icpc2022x.jpg": cert4,
+    "javascript.jpg": cert5,
+    "css.jpg": cert6,
+    "freelancer.jpg": cert7,
+    "responsive-freecodecamp.png": cert8,
+    "javascript-freecodecamp.png": cert9,
+    "frontend-freecodecamp.png": cert10
+};
