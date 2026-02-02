66f822dd76ecab84985eff907471a5cb155c52dc
Bola Nabil <bolanabil404@gmail.com>
2026-02-02 15:28:28 +0200

    add experience container and item

--- src/utilts/animations.js
+++ src/utilts/animations.js
@@ -17,3 +17,16 @@ export const aboutItem = {
   hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0 },
 }
+
+export const experienceContainer = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+export const experienceItem = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 },
+};
