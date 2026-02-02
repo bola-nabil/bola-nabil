63b8976caf0c9b54a4e518bf63fbae03c6fe6b56
Bola Nabil <bolanabil404@gmail.com>
2026-02-02 13:10:05 +0200

    add animations and effects

---
+++ src/utilts/animations.js
@@ -0,0 +1,19 @@
+export const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+export const homeItem = {
+  hidden: { opacity: 0, y: 10 },
+  show: { opacity: 1, y: 0 },
+};
+
+export const aboutItem = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 },
+}
