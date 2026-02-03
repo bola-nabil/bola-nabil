6a6e91308715389590577ee45f724dc91c6142f6
Bola Nabil <bolanabil404@gmail.com>
2026-02-03 14:09:44 +0200

    loading

---
+++ src/components/ui/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className='loading center-row'>
+      <div className=''>
+        <div className="spinner"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading;
