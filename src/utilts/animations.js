888b6ebbd3ecce844f42481bfc64e9fdfaea0019
Bola Nabil <bolanabil404@gmail.com>
2026-02-03 13:37:56 +0200

    add export const containerVariants = {

--- src/utilts/animations.js
+++ src/utilts/animations.js
@@ -43,3 +43,13 @@ export const cardVariants = {
   hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0 },
 };
+
+export const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
