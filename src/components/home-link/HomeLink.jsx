dedf5ea89016eb11dc6d2f13f895c3e3e167e76f
Bola Nabil <bolanabil404@gmail.com>
2026-02-03 14:56:24 +0200

    add aria to links

--- src/components/home-link/HomeLink.jsx
+++ src/components/home-link/HomeLink.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./home-link.css";
 
-const HomeLink = ({ to, icon }) => {
+const HomeLink = ({ to, icon, aria}) => {
   return (
     <>
-        <Link to={to} target="_blank">
+        <Link to={to} target="_blank" aria-label={aria}>
           <div className="home-icon center-row bg-black">
             <FontAwesomeIcon className="icon" icon={icon} />
           </div>
