2c277b7b10a57577c524d80b6c535ae1a161e94e
Bola Nabil <bolanabil404@gmail.com>
2026-05-17 17:32:30 +0300

    add RouterProvider to manage router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
