a3b6531c4b9974d642d353ff0aeb161bf3c784f8
Bola Nabil <bolanabil404@gmail.com>
2026-05-17 17:33:20 +0300

    remove routes and route, using router.jsx instead

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React from "react";
-import Home from "./pages/Home/Home.jsx";
-import About from "./pages/About/About.jsx";
-import Resume from "./pages/Resume/Resume.jsx";
-import Projects from "./pages/Projects/Projects.jsx";
-import Contact from "./pages/Contact/Contact.jsx";
-import NotFound from "./pages/NotFound";
-import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/layout/NavBar/NavBar.js";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
     </>
   );
 };
